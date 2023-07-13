a8804f688f25840b4194af14308dd13287143d90
Glitch (jan-softa-quote-server) <none>
2023-07-13 12:45:29 +0000

    🔧〽️ Updated with Glitch

--- server.js
+++ server.js
@@ -8,25 +8,26 @@ const app = express();
 //load the quotes JSON
 const quotes = require("./quotes.json");
 
-// Now register handlers for some routes:
-//   /                  - Return some helpful welcome info (text)
-//   /quotes            - Should return all quotes (json)
-//   /quotes/random     - Should return ONE quote (json)
-app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
-});
 
-//START OF YOUR CODE...
+START OF YOUR CODE...
+app.get('', (req, res) => {
+  res.send(quotes);
+})
+
+
 
 //...END OF YOUR CODE
 
-//You can use this function to pick one element at random from a given array
-//example: pickFromArray([1,2,3,4]), or
-//example: pickFromArray(myContactsArray)
 //
 function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
+
+app.get("/quotes/random", (req, res) => {
+ 
+  res.send(pickFromArray(quotes));
+});
+
 
 //Start our server so that it listens for HTTP requests!
 const listener = app.listen(process.env.PORT, function () {
